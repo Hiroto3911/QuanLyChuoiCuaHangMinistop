57c85e09d537fbf72275fe7f39ec026cc99a9d5f
Hiroto3911 <hirotokuga3911@gmail.com>
2025-05-04 22:12:54 +0700

    Cập nhập sql và chức năng bán hàng, khuyến mãi

--- QuanLyChuoiCuaHangTienLoi_Ministop/BUS/BUS_KhuyenMai_SanPham.cs
+++ QuanLyChuoiCuaHangTienLoi_Ministop/BUS/BUS_KhuyenMai_SanPham.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using ET;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace BUS
 {
-    internal class BUS_KhuyenMai_SanPham
+    public class BUS_KhuyenMai_SanPham
     {
+        private DAL_KhuyenMai_SanPham dal_km_sp = new DAL_KhuyenMai_SanPham();
+        public List<ET_KhuyenMai_SanPham> HienThiDuLieuMotKMSapXepGiamDanTheoMaKM(string maKM)
+        {
+
+           
+            return dal_km_sp.HienThiDuLieuMotKMSapXepGiamDanTheoMaKM(maKM);
+        }
+        public bool Them(ET_KhuyenMai_SanPham ctkm)
+        {
+            return dal_km_sp.Them(ctkm);
+        }
+
+        public bool Xoa(string maKM, string maSP)
+        {
+            return dal_km_sp.Xoa(maKM, maSP);
+        }
+        public bool Sua(ET_KhuyenMai_SanPham km)
+        {
+            return dal_km_sp.Sua(km);
+        }
+        public bool KiemTraDoUuTien(ET_KhuyenMai_SanPham ctkm)
+        {
+           return dal_km_sp.KiemTraDoUuTien(ctkm);
+
+        }
+        public ET_KhuyenMaiApDung ApDungKhuyenMaiChoSanPham(ET_ChiTietHoaDon cthd, DateTime tgianMua)
+        {
+           return dal_km_sp.ApDungKhuyenMaiChoSanPham(cthd, tgianMua);
+        }
     }
 }
---
+++ QuanLyChuoiCuaHangTienLoi_Ministop/ET/ET_KhuyenMaiApDung.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public class ET_KhuyenMaiApDung
+    {
+        public string MaSanPham { get; set; }
+        public string TenKhuyenMai { get; set; }
+        public decimal MucGiamGia { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+       
+
+    }
+}
