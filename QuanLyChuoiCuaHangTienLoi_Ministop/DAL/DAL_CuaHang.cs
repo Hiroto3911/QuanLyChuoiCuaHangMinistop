37c1aaf53624b665478492245726d412b0254383
Hiroto3911 <hirotokuga3911@gmail.com>
2025-05-05 20:08:58 +0700

    Cập nhập chức năng quản lý cửa hàng

--- QuanLyChuoiCuaHangTienLoi_Ministop/DAL/DAL_CuaHang.cs
+++ QuanLyChuoiCuaHangTienLoi_Ministop/DAL/DAL_CuaHang.cs
@@ -27,19 +27,71 @@ namespace DAL
             }
             return ds;
         }
-        //public bool Them(ET_HoaDon hoaDon)
-        //{
-            
-        //}
+        public bool Them(ET_CuaHang cuaHang)
+        {
+            if (cuaHang == null) return false;
+            try
+            {
+                CuaHang ch = new CuaHang()
+                {
 
-        //public bool Xoa(string maHD)
-        //{
-           
-        //}
-        //public HoaDon TimHoaDonBangMaHD(string maHD)
-        //{
-           
-        //}
+                    MaCuaHang = cuaHang.MaCH,
+                    TenCuaHang = cuaHang.TenCH,
+                    SoDienThoai = cuaHang.SDT,
+                    DiaChi = cuaHang.DChi
+                };
+                db.CuaHangs.InsertOnSubmit(ch);
+                db.SubmitChanges();
+                return true;
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+        public bool Xoa(string maCH)
+        {
+            if (string.IsNullOrWhiteSpace(maCH)) return false;
+            try
+            {
+                var del = TimCuaHangBangMaCuaHang(maCH);
+                if (del == null) return false;
+                db.CuaHangs.DeleteOnSubmit(del);
+                db.SubmitChanges();
+                return true;
+            }
+            catch (Exception ex) { throw ex; }
+        }
+        public bool Sua(ET_CuaHang cuaHang)
+
+        {
+
+            if (cuaHang == null) return false;
+            try
+            {
+                var capNhat = TimCuaHangBangMaCuaHang(cuaHang.MaCH);
+                if (capNhat == null) return false;
+                capNhat.TenCuaHang = cuaHang.TenCH;
+                capNhat.SoDienThoai = cuaHang.SDT;
+                capNhat.MaQuanLy = cuaHang.NguoiQL;
+                capNhat.DiaChi = cuaHang.DChi;
+                db.SubmitChanges();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+
+
+                throw ex;
+
+            }
+
+        }
+
+        public CuaHang TimCuaHangBangMaCuaHang(string maCH)
+        {
+            var kq = db.CuaHangs.Where(n => n.MaCuaHang == maCH).FirstOrDefault();
+            return kq;
+        }
         public List<string> LayDanhSachMaCH()
         {
             var danhSachID = db.CuaHangs.Select(id => id.MaCuaHang).ToList();
